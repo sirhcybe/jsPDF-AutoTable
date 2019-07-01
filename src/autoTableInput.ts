c216af539639f3af2e20e0664f6436547d0e6dc2
Christopher Eby <ceby@geodecisions.com>
2019-07-01 11:02:47 -0400

    Fixing bug when value setting had more than one word by changing functionality to match PDF spec

--- src/autoTableInput.ts
+++ src/autoTableInput.ts
@@ -62,8 +62,7 @@ jsPDF.API.autoTableInput = function (
         let checkBox = new CheckBox();
         checkBox.fieldName = fieldName;
         checkBox.Rect = [x, y + lineHeight, lineHeight, lineHeight];
-        checkBox.appearanceState = value === options[0] ? 'On' : 'Off';
-        checkBox.value = value;
+        checkBox.value = value === options[0] ? 'On' : 'Off';
         checkBox.caption = '5';
         checkBox.maxFontSize = this.internal.getFontSize();
         this.addField(checkBox);
